76493d0fd991578f72a191af361376c8351d03c6
Karshan Sharma <karshan.sharma@gmail.com>
2016-07-21 13:50:19 -0400

    festival-registration form post

--- js/festival-registration.js
+++ js/festival-registration.js
@@ -32,28 +32,64 @@ var main = function() {
         $("#festival-masterclass-questions").hide();
     });
 
-    // default view
+    // default view, clicking once doesn't work.
+    // possibly because not using $('input[name='...']).click(function...) for radio button click handler
+    $("#festival-radio-button").click();
     $("#festival-radio-button").click();
 };
 
-function submitForm() {
-  var radio_button = $('input[name=eventType]:checked')[0].id;
-  var eventName = $('#event-name').val();
-  var startDate = $('#datetimepicker1')[0].children[0].value;
-  var endDate = $('#datetimepicker2')[0].children[0].value;
-  var eventCountry = $('#event-country').val();
-  var eventCity = $('#event-city').val();
-  var eventDescription = $('#event-description').val();
-  var schedule = $('#schedule').val();
+function lolcat() {
+  var masterclass_radio = $('input[name=whether-masterclasses]:checked')[0];
+  var toSend = {
+    radio_button : $('input[name=eventType]:checked')[0].id,
+    eventName : $('#event-name').val(),
+    startDate : $('#datetimepicker1')[0].children[0].value,
+    endDate : $('#datetimepicker2')[0].children[0].value,
+    eventCountry : $('#event-country').val(),
+    eventCity : $('#event-city').val(),
+    eventDescription : $('#event-description').val(),
+    schedule : $('#schedule').val(),
 
-  // festival questions
-  var guestList = $('#guest-list').val();
-  var master_class = $('input[name=whether-masterclasses]:checked')[0].id
+    // practical information
+    eventFees : $('#event-fees').val(),
+    howToRegister : $('#how-to-register').val(),
+    contactInformation : $('#contact-information').val(),
+    venueAddresses : $('#venue-addresses').val(),
+    accomodation : $('#accomodation').val(),
+    travelInfo : $('#travel-info').val(),
+    otherFacilities : $('#other-facilities').val(),
 
-  // festival-masterclass
-  var master_class_teachers = $('#masterclass-teachers').val();
-  var master_class_pricing = $('#masterclass-pricing').val();
-  
+    // festival questions
+    guestList : $('#guest-list').val(),
+    masterclass : masterclass_radio == undefined ? 'undefined' : masterclass_radio.id,
+
+    // masterclass
+    masterclassTeachers : $('#masterclass-teachers').val(),
+    masterclassPricing : $('#masterclass-pricing').val(),
+
+    // competition questions
+    competitionRules : $('#competition-rules').val(),
+    prizes : $('#prizes').val(),
+    jury : $('#jury').val(),
+
+    // workshop questions
+    workshopTeachers : $('#workshop-teachers').val(),
+    workshopAccomodation : $('#workshop-accomodation').val(),
+    workshopMeals : $('#workshop-meals').val(),
+    workshopPracticeFacilities : $('#workshop-practice-facilities').val(),
+    workshopLessons : $('#workshop-lessons').val(),
+    workshopOtherGuests : $('#workshop-other-guests').val(),
+  };
+
+  $.ajax({
+      method: "POST",
+      url: "registerFestival",
+      data: JSON.stringify(toSend),
+      processData: false,
+  }).done(function(msg) {
+      alert('posted, returned: ' + msg);
+  });
+  return false;
 }
 
 $(document).ready(main);
