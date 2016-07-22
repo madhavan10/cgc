30f2104f898469763eb97868f0b465f6303bcd24
Madhavan Somanathan <madhavansomanathan@gmail.com>
2016-07-22 03:13:18 +0200

    mistake

--- js/gc-check-user.js
+++ js/gc-check-user.js
@@ -1,7 +1,7 @@
 var main = function() {
    if(!(window.user === undefined)) {
         $('#gc-login-link').text(window.user.firstName);
-        $('#gc.login-link').attr('href', "login.html");
+        $('#gc-list-your-own-link').attr('href', "login.html");
    }
 };
 
