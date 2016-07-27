1c31cd368241a2a84175caa060ff723b84a874f7
Madhavan Somanathan <madhavansomanathan@gmail.com>
2016-07-27 02:54:51 +0200

    Added js script to fill out fields in description page

---
+++ js/description-page.js
@@ -0,0 +1,49 @@
+var main = function() {
+   var info = JSON.parse(festival._rawJSON);
+    
+   $('#gc-festival-name').text(info.eventName);
+    
+    $('#town').text(info.eventCity);
+    $('#country').text(info.eventCountry);
+    
+    var month = new Date(info.startDate.getMonth());
+    var date = new Date(info.startDate.getDay());
+    var year = new Date(info.startDate.getYear());
+    $('#month').text(month);
+    $('#date').text(date);
+    $('#year').text(year);
+    
+    $('#gc-description-text').text(info.eventDescription);
+
+    $('#schedule-content').text(info.schedule);
+    $('#guests-content').text(info.guestList);
+    $('#masterclasses-content').text(info.masterclassTeachers + "\n\n" 
+            + info.masterclassPricing);
+    
+    $('#competition-content').text(info.competitionRules + "\n\n" 
+            + info.prizes + "\n\n" 
+            + info.jury);
+    
+    //competition.html
+    $('#rules-content').text(info.competitionRules);
+    $('#prizes-content').text(info.prizes);
+    $('#jury-content').text(info.jury);
+
+    //workshop.html
+    $('#teachers-content').text(info.workshopTeachers);
+    $('#lessons-content').text(info.workshopLessons);
+    $('#other-guests-content').text(info.workshopOtherGuests);
+
+
+    $('#practical-info-content').text(info.eventFees + "\n\n"  
+            + info.venueAddresses + "\n\n"
+            + info.accomodation + "\n\n"
+            + info.travelInfo + "\n\n"
+            + info.otherFacilities);
+    $('#contact-content').text(info.contactInformation);
+    $('#register-content').text(info.howToRegister);
+
+            
+};
+
+$(document).ready(main);
