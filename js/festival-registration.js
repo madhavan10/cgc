f95fe46bbc075d32d30304848d0fe7b432371173
Madhavan Somanathan <madhavansomanathan@gmail.com>
2016-07-28 02:08:00 +0200

    removed redundant question about accommodation for workshops

--- js/festival-registration.js
+++ js/festival-registration.js
@@ -4,23 +4,24 @@ var main = function() {
     $("#practical-information-questions").show();
     $(".gc-event-radio").click(function() {
         if($("#festival-radio-button").is(":checked")) {
-            $(".gc-event-questions").hide()    
+            $(".gc-event-questions").hide();    
             $("#festival-questions").show();
         }
         
         if($("#competition-radio-button").is(":checked")) {
-            $(".gc-event-questions").hide()    
+            $(".gc-event-questions").hide();   
             $("#competition-questions").show();
         }
         
         if($("#festival-competition-radio-button").is(":checked")) {
-            $(".gc-event-questions").hide()    
+            $(".gc-event-questions").hide();   
             $("#festival-questions").show();
             $("#competition-questions").show();
         }
 
         if($("#workshop-radio-button").is(":checked")) {
-            $(".gc-event-questions").hide()    
+            $(".gc-event-questions").hide();
+            $(".workshop-redundant").hide();
             $("#workshop-questions").show();
         }
     });
