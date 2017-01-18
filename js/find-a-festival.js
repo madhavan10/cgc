5aa6eae15b8094b81e1012c514713d501f6b79cf
Madhavan Somanathan <madhavansomanathan@gmail.com>
2017-01-18 15:45:29 +0100

    Added a bunch of a new files and modified header

---
+++ js/find-a-festival.js
@@ -0,0 +1,8 @@
+var main = function() {
+    
+    for(var i = 0; i < window.festivals.length; ++i) {
+       var thisEvent = JSON.parse(window.festivals[i]._rawJSON);
+       thisEvent.name
+    }
+}
+$(document).ready(main);
