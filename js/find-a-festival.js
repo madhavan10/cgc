86db4d7b80d08314cfe2d39a44f41329ac419909
Madhavan Somanathan <madhavansomanathan@gmail.com>
2017-01-18 16:35:21 +0100

    Find a festival now prints a list of registered festivals

--- js/find-a-festival.js
+++ js/find-a-festival.js
@@ -1,8 +1,14 @@
 var main = function() {
     
+    var arrayOfNames = [];
     for(var i = 0; i < window.festivals.length; ++i) {
        var thisEvent = JSON.parse(window.festivals[i]._rawJSON);
-       thisEvent.name
+       arrayOfNames.push(thisEvent.eventName);
+    }
+    for(var i = 0; i < arrayOfNames.length; ++i) {
+        $(".gc-find-a-festival").append("<div>");
+        $(".gc-find-a-festival").append(arrayOfNames[i]);
+        $(".gc-find-a-festival").append("</div>");
     }
 }
 $(document).ready(main);
