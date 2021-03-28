9aaefc454a4c812a8ac792ae460f5de2428043fb
Madhavan Somanathan <madhavansomanathan@gmail.com>
2021-03-28 18:12:27 +0530

    clicking on the label text for radio buttons now works.

--- js/festival-registration.js
+++ js/festival-registration.js
@@ -52,51 +52,49 @@ var main = function() {
     // possibly because not using $('input[name='...']).click(function...) for radio button click handler
     $("#festival-radio-button").click();
     $("#festival-radio-button").click();
-
-    $("textarea").val().replace(/\r\n/g, "<br />");
 };
 
 function submitFestivalRegistrationForm() {
-  var masterclass_radio = $('input[name=whether-masterclasses]:checked')[0];
-  var toSend = {
-    radio_button : $('input[name=eventType]:checked')[0].id,
-    eventName : $('#event-name').val(),
-    startDate : $('#datetimepicker1')[0].children[0].value,
-    endDate : $('#datetimepicker2')[0].children[0].value,
-    eventCountry : $('#event-country').val(),
-    eventCity : $('#event-city').val(),
-    eventDescription : $('#event-description').val(),
-    schedule : $('#schedule').val(),
+    var masterclass_radio = $('input[name=whether-masterclasses]:checked')[0];
+    var toSend = {
+        radio_button : $('input[name=eventType]:checked')[0].id,
+        eventName : $('#event-name').val(),
+        startDate : $('#datetimepicker1')[0].children[0].value,
+        endDate : $('#datetimepicker2')[0].children[0].value,
+        eventCountry : $('#event-country').val(),
+        eventCity : $('#event-city').val(),
+        eventDescription : $('#event-description').val(),
+        schedule : $('#schedule').val(),
 
-    // practical information
-    eventFees : $('#event-fees').val(),
-    howToRegister : $('#how-to-register').val(),
-    contactInformation : $('#contact-information').val(),
-    venueAddresses : $('#venue-addresses').val(),
-    accommodation : $('#accommodation').val(),
-    travelInfo : $('#travel-info').val(),
-    otherFacilities : $('#other-facilities').val(),
+        // practical information
+        eventFees : $('#event-fees').val(),
+        howToRegister : $('#how-to-register').val(),
+        contactInformation : $('#contact-information').val(),
+        venueAddresses : $('#venue-addresses').val(),
+        accommodation : $('#accommodation').val(),
+        travelInfo : $('#travel-info').val(),
+        otherFacilities : $('#other-facilities').val(),
 
-    // festival questions
-    guestList : $('#guest-list').val(),
-    masterclass : masterclass_radio == undefined ? 'undefined' : masterclass_radio.id,
+        // festival questions
+        guestList : $('#guest-list').val(),
+        masterclass : masterclass_radio == undefined ? 'undefined' : masterclass_radio.id,
 
-    // masterclass
-    masterclassTeachers : $('#masterclass-teachers').val(),
-    masterclassPricing : $('#masterclass-pricing').val(),
+        // masterclass
+        masterclassTeachers : $('#masterclass-teachers').val(),
+        masterclassPricing : $('#masterclass-pricing').val(),
 
-    // competition questions
-    competitionRules : $('#competition-rules').val(),
-    prizes : $('#prizes').val(),
-    jury : $('#jury').val(),
+        // competition questions
+        competitionRules : $('#competition-rules').val(),
+        prizes : $('#prizes').val(),
+        jury : $('#jury').val(),
 
-    // workshop questions
-    workshopTeachers : $('#workshop-teachers').val(),
-    workshopAccommodation : $('#workshop-accommodation').val(),
-    workshopMeals : $('#workshop-meals').val(),
-    workshopPracticeFacilities : $('#workshop-practice-facilities').val(),
-    workshopLessons : $('#workshop-lessons').val(),
-    workshopOtherGuests : $('#workshop-other-guests').val(),
+        // workshop questions
+        workshopTeachers : $('#workshop-teachers').val(),
+        workshopAccommodation : $('#workshop-accommodation').val(),
+        workshopMeals : $('#workshop-meals').val(),
+        workshopPracticeFacilities : $('#workshop-practice-facilities').val(),
+        workshopLessons : $('#workshop-lessons').val(),
+        workshopOtherGuests : $('#workshop-other-guests').val(),
   };
 
   $.ajax({
