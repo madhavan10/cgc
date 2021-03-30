e41891b4e83bd3a5c4453d70df3fab056b3bc0c2
Madhavan Somanathan <madhavansomanathan@gmail.com>
2021-03-30 12:49:32 +0530

    Adds a class to mark each listing as festival, competition or workshop (for filtering), shows events alphabetically.

--- js/find-a-festival.js
+++ js/find-a-festival.js
@@ -1,12 +1,26 @@
 var main = function() {
     
-    var arrayOfNames = [];
+    var eventList = [];
     for(var i = 0; i < window.festivals.length; ++i) {
        var thisEvent = JSON.parse(window.festivals[i]._rawJSON);
-       arrayOfNames.push(thisEvent.eventName);
+       eventList.push(thisEvent);
     }
-    for(var i = 0; i < arrayOfNames.length; ++i) {
-        $(".gc-find-a-festival").append("<div><a href=\"/festivals/" + arrayOfNames[i] + "\">" + arrayOfNames[i] + "</a></div>");
+    //sort alphabetically
+    eventList.sort(function (e1, e2) {
+        return (e1.eventName).localeCompare(e2.eventName);
+    });
+    for(var i = 0; i < eventList.length; ++i) {
+        var className;
+        if(eventList[i].radio_button === "festival-radio-button") {
+            className = "gc-festival-listing";
+        } else if (eventList[i].radio_button === "competition-radio-button") {
+            className = "gc-competition-listing";
+        } else if (eventList[i].radio_button === "festival-competition-radio-button") {
+            className = "gc-festival-listing gc-competition-listing";
+        } else {
+            className = "gc-workshop-listing";
+        }
+        $(".gc-find-a-festival").append("<div class=\"" + className + "\"><a href=\"/festivals/" + eventList[i].eventName + "\">" + eventList[i].eventName + "</a></div>");
     }
 }
 $(document).ready(main);
