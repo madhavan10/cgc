65444f5c7a6ae9549f3efb4c6a94df8c8e303b71
Madhavan Somanathan <madhavansomanathan@gmail.com>
2021-04-15 15:24:32 +0530

    Find Festival by list page works with two filters, eventType and eventDate (upcoming or all). Newlines are now displayed in the content of the mobile-width version of the description page.

--- js/find-a-festival.js
+++ js/find-a-festival.js
@@ -9,36 +9,90 @@ var main = function() {
     eventList.sort(function (e1, e2) {
         return (e1.eventName).localeCompare(e2.eventName);
     });
+        
+    var now = new Date();
     for(var i = 0; i < eventList.length; ++i) {
-        var className;
-        if(eventList[i].radio_button === "festival-radio-button") {
-            className = "gc-festival-listing gc-event-listing";
-        } else if (eventList[i].radio_button === "competition-radio-button") {
-            className = "gc-competition-listing gc-event-listing";
-        } else if (eventList[i].radio_button === "festival-competition-radio-button") {
-            className = "gc-festival-listing gc-competition-listing gc-event-listing";
-        } else {
-            className = "gc-workshop-listing gc-event-listing";
+        var className = "";
+        
+        //event temporality
+        //Note: event dates exclude Time Zone
+        //Note: Use of Date constructor with timestring argument is not encouraged
+        if (new Date(eventList[i].endDate) >= now) {
+            className += " gcm-upcoming-listing ";
         }
+
+        //event type
+        if(eventList[i].radio_button === "festival-radio-button") {
+            className += " gc-festival-listing ";
+        } else if (eventList[i].radio_button === "competition-radio-button") {
+            className += " gc-competition-listing ";
+        } else if (eventList[i].radio_button === "festival-competition-radio-button") {
+            className += " gc-festival-listing gc-competition-listing ";
+        } else {
+            className += " gc-workshop-listing ";
+        }
+
+        className += " gc-event-listing ";
         $("#gc-festival-list").append("<div class=\"" + className + "\"><a href=\"/festivals/" + eventList[i].eventName + "\">" + eventList[i].eventName + "</a></div>");
     }
+    
+    $(".gc-event-listing").hide();
+    var e;
+    for (e of $(".gcm-upcoming-listing")) {
+        e.classList.add("gcm-eventDate-active");
+    }
+    for (e of $(".gc-event-listing")) {
+        e.classList.add("gcm-eventType-active");
+    }
+    $(".gcm-eventType-active.gcm-eventDate-active").show();
 };
 
 var selectEventType = function(value) {
+    $(".gc-event-listing").hide();
+    var e;
+    for (e of $(".gc-event-listing")) {
+        e.classList.remove("gcm-eventType-active");
+    }
     if (value==="festival") {
-        $(".gc-event-listing").hide();
-        $(".gc-festival-listing").show();
+        for (e of $(".gc-festival-listing")) {
+            e.classList.add("gcm-eventType-active");
+        }
     } else if (value==="competition") {
-        $(".gc-event-listing").hide();
-        $(".gc-competition-listing").show();
+        for (e of $(".gc-competition-listing")) {
+            e.classList.add("gcm-eventType-active");
+        }
     } else if (value==="workshop") {
-        $(".gc-event-listing").hide();
-        $(".gc-workshop-listing").show();
+        for (e of $(".gc-workshop-listing")) {
+            e.classList.add("gcm-eventType-active");
+        }
     } else if (value==="All") {
-        $(".gc-event-listing").show();
+        for (e of $(".gc-event-listing")) {
+            e.classList.add("gcm-eventType-active");
+        }
     } else {
         alert(value);
     }
+    $(".gcm-eventType-active.gcm-eventDate-active").show();
+};
+
+var selectEventsAllOrNew = function (value) {
+    $(".gc-event-listing").hide();
+    var e;
+    for (e of $(".gc-event-listing")) {
+        e.classList.remove("gcm-eventDate-active");
+    }
+    if (value === "upcoming") {
+        for (e of $(".gcm-upcoming-listing")) {
+            e.classList.add("gcm-eventDate-active");
+        }
+    } else if (value === "all") {
+        for (e of $(".gc-event-listing")) {
+            e.classList.add("gcm-eventDate-active");
+        }
+    } else {
+        alert(value);
+    }
+    $(".gcm-eventDate-active.gcm-eventType-active").show();
 };
 
 $(document).ready(main);
