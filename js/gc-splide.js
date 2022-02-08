565ddd7ddc087c3a70bbaab9108e1a9784b4b46e
Bogdan <bogdan_mihailescu15@yahoo.com>
2022-02-08 17:22:39 +0100

    styled slider

--- js/gc-splide.js
+++ js/gc-splide.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     new Splide(".splide", {
+        updateOnMove: true,
         cover:true, 
-        heightRatio:0.45
+        heightRatio:0.50,
+        type: 'loop',
+        padding: '12rem',
+        gap: '1.5rem',
+        autoplay: true,
+        
+        
     }).mount();
 });
+
+
